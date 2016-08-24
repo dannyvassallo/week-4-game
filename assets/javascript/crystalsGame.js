75ed953705d49e14162fc5cfd7e6fc1062a43133
Mario <Mario>
2016-08-24 01:55:03 -0400

    crystal game without css styling

---
+++ assets/javascript/crystalsGame.js
@@ -0,0 +1,19 @@
+// $( document ).ready(function () {
+// 	var randomNumber = "";
+// 	var wins = 0;
+// 	var losses = 0;
+// 	var totalScore = 0;
+// 	var numberToGuess = "";
+// 	var counter1 = "";
+// 	var counter2 = "";
+// 	var counter3 = "";
+// 	var counter4 = "";
+
+
+
+// 	function getRandomInt(min, max) {
+//   		return Math.floor(Math.random() * (max - min + 1)) + 1;
+// 		numberToGuess = getRandomInt(19,120)
+// 	}
+// 	$('#numberToGuess').text(numberToGuess);
+// });
